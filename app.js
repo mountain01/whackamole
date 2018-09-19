e9ea0194d0bf703fe11cdbd37b3768342f4a8c2e
Andrew Tate <drewctate@gmail.com>
2018-09-19 13:41:02 -0600

    Basic Game Play

--- app.js
+++ app.js
@@ -1,22 +1,57 @@
-function init() {
-  const stage = new createjs.Stage('game-canvas');
+let mole;
+let stage;
+let lastTime = 0;
+let score = 0;
 
-  const mole = createMole('hard');
-  mole.x = stage.canvas.width/2;;
-  mole.y = stage.canvas.height/2;
-  mole.scale = 3;
+function init() {
+  stage = new createjs.Stage('game-canvas');
+
+  mole = createMole('hard');
+  mole.x = stage.canvas.width / 2;;
+  mole.y = stage.canvas.height / 2;
+  mole.scale = .5;
+
+  mole.addEventListener('click', bopMole);
 
   stage.addChild(mole);
 
   stage.update();
+
+  createjs.Ticker.addEventListener("tick", handleTick);
 }
 
-function createMole(level){
+function bopMole() {
+  score++;
+  updateScore(score);
+  randomizeMole();
+  lastTime = createjs.Ticker.getTime();
+  stage.update();
+}
+
+function updateScore(score) {
+  document.getElementById('score').innerText = score;
+}
+
+function randomizeMole() {
+  mole.x = Math.floor(Math.random() * stage.canvas.width);
+  mole.y = Math.floor(Math.random() * stage.canvas.height);
+}
+
+function handleTick() {
+  const time = createjs.Ticker.getTime();
+  if (time - lastTime > 1000) {
+    lastTime = time;
+    randomizeMole();
+    stage.update();
+  }
+}
+
+function createMole(level) {
   const mole = new createjs.Container();
 
   let headColor, eyeColor, noseColor, mouthColor, pupilColor;
 
-  switch(level){
+  switch (level) {
     case 'hard':
       headColor = 'black';
       eyeColor = 'yellow';
@@ -42,8 +77,8 @@ function createMole(level){
   nose.y = 10;
 
   const mouth = createMouth(mouthColor);
-  mouth.scale=.5;
-  mouth.y=22;
+  mouth.scale = .5;
+  mouth.y = 22;
   mouth.x = -12.5;
 
   mole.addChild(head, eyes, nose, mouth);
@@ -52,40 +87,40 @@ function createMole(level){
   return mole;
 }
 
-function createMouth(color){
+function createMouth(color) {
   const mouth = new createjs.Shape();
-  mouth.graphics.beginFill(color).drawEllipse(0,0,50,35);
+  mouth.graphics.beginFill(color).drawEllipse(0, 0, 50, 35);
   return mouth;
 }
 
 function createNose(color) {
   const nose = new createjs.Shape();
-  nose.graphics.beginFill(color).drawCircle(0,0,50);
+  nose.graphics.beginFill(color).drawCircle(0, 0, 50);
   return nose;
 }
 
-function createHead(color){
+function createHead(color) {
   const head = new createjs.Shape();
-  head.graphics.beginFill(color).drawCircle(0,0,50);
+  head.graphics.beginFill(color).drawCircle(0, 0, 50);
   return head;
 }
-function createEyes(color,pupil){
+function createEyes(color, pupil) {
   const eyes = new createjs.Container();
   const eye1 = createEye(color, pupil);
   const eye2 = createEye(color, pupil);
   eye2.x = 150;
 
-  eyes.addChild(eye1,eye2);
+  eyes.addChild(eye1, eye2);
 
   return eyes;
 }
 
-function createEye(color,pupil){
+function createEye(color, pupil) {
   const eye = new createjs.Container();
   const part1 = new createjs.Shape();
-  part1.graphics.beginFill(color).drawCircle(0,0,50);
+  part1.graphics.beginFill(color).drawCircle(0, 0, 50);
   const part2 = new createjs.Shape();
-  part2.graphics.beginFill(pupil).drawCircle(0,0,50);
+  part2.graphics.beginFill(pupil).drawCircle(0, 0, 50);
   part2.scale = .5;
   eye.addChild(part1);
   eye.addChild(part2);
